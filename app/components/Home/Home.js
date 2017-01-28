9e9666cf8a36d5fa6061d933770e44df0cb9b586
rosemerryberry <adamsrc@uw.edu>
2017-01-28 14:31:18 -0800

    edited description

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -8,8 +8,8 @@ import VerticalTimeline from '../verticalTimeline/verticalTimeline'
 
 
 //  Temporary component for concept only. Self containing structure.
-const title = 'The tool to keep the executive branch in check'
-const text = 'This is a React.JS project to create an "issue tracker" for the campaign promises of the Trump-Pence administration. This will track developments in presidential initiatives, as well as any counter-movements sprung from citizens and opposing parties. Thus, we create an information system that is more than just another "president tracker" to play up headlines - we create a tool for eliniminating the noise and mobilizing the public.\n\nAn ideal use case for this website would be to learn about trump and his initiatives, where our government stands, and what citizens can do in response. Existing issue trackers either cover one side of the story(trump but not pence, protests but not executive orders, etc) and are an incomplete information system. Let\'s close the loop.'
+const title = 'Welcome to Executive Branch Tracker'
+const text = 'The mission of this site is to provide an unbiased record of administrative action taken by the executive branch and the subsequent responses organized by the American public. We want to inform users about the facts of the Trump administration and empower those who may feel helpless.'
 
 const Home = ({data}) => (
   <div>
