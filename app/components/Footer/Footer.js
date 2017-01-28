9ebacac1e7fdb7d4f4c798dfd7f909b31925a7bd
redhawkIT <khodkodmitry10011983@hotmail.com>
2017-01-28 09:06:06 -0800

    Adding sidebar links. Will make those categories w/ routes.

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -9,8 +9,8 @@ import {CardHeader} from 'material-ui/Card'
 const Footer = () => (
   <Paper style={C.Theme.footer}>
     <CardHeader style={C.Normalize}
-      title='© Ryan Keller, Rosemary Adams, team member B'
-      subtitle='UW iSchool WINFO Hackathon'
+      title='© Keller, Rosemary, Jessica, Benjamin & McKaulay'
+      subtitle='UW iSchool WINFO Hackathon 2017'
       avatar='https://firebasestorage.googleapis.com/v0/b/presidential-io.appspot.com/o/ischool-symbol-only-purple-web.png?alt=media&token=bcd4ff2b-991d-4cb8-a18a-8e28b143da40'
     />
   </Paper>
--- app/components/Sidebar/Sidebar.js
+++ app/components/Sidebar/Sidebar.js
@@ -7,9 +7,17 @@ import React from 'react'
 import {List, ListItem} from 'material-ui/List'
 import Divider from 'material-ui/Divider'
 
+import ContentInbox from 'material-ui/svg-icons/content/inbox'
+import ActionGrade from 'material-ui/svg-icons/action/grade'
+import ContentSend from 'material-ui/svg-icons/content/send'
+import ContentDrafts from 'material-ui/svg-icons/content/drafts'
+
 const Sidebar = ({github = C.Initial.github}) =>
   <List style={C.Theme.list}>
-    <div />
+    <ListItem primaryText='Inbox' leftIcon={<ContentInbox />} />
+    <ListItem primaryText='Starred' leftIcon={<ActionGrade />} />
+    <ListItem primaryText='Sent mail' leftIcon={<ContentSend />} />
+    <ListItem primaryText='Drafts' leftIcon={<ContentDrafts />} />
     <Divider />
     <a target='_blank' href={github}>
       <ListItem style={C.Theme.link}
