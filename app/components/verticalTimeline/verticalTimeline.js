e383933573ec0c43de9a72e70fe2ce2285d8551e
Jessica Libman <jlibman@gmail.com>
2017-01-28 14:51:32 -0800

    vertical timeline is populated by firebase

--- app/components/verticalTimeline/verticalTimeline.js
+++ app/components/verticalTimeline/verticalTimeline.js
@@ -10,44 +10,31 @@ import {Row, Col} from 'react-grid-system'
 
 const VerticalTimeline = ({data}) => (
   <Col style={C.Normalize} sm={12}>
-    <ul className='verticalTimeline'>
 
-      <li className='verticalTimeline-item'>
-        <div className='verticalTimeline-image red'>
-          <span className='glyphicon glyphicon-th-large' aria-hidden='true' />
-        </div>
-        <div className='verticalTimeline-content'>
-          <h3 className='title'>Lorem ipsum dolor sit amet</h3>
-          <Row>
-            <Col sm={3}>
-              <img src='https://cretoniatimesdotcom.files.wordpress.com/2015/07/donald-trump-bad-hair-photo-1.jpg?w=640' alt='trump sucks' />
-            </Col>
-            <Col sm={9}>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum non nisl dapibus est gravida ornare. Nullam laoreet lacus pretium ante dignissim, eu luctus risus interdum.</p>
-            </Col>
-          </Row>
-        </div>
-      </li>
+    <ul className="verticalTimeline">
+    {data.headlines && data.headlines.map((line, i) => (
 
-      <li className='verticalTimeline-item'>
-        <div className='verticalTimeline-image yellow'>
-          <span className='glyphicon glyphicon-tint' aria-hidden='true' />
-        </div>
-        <div className='verticalTimeline-content'>
-          <h3 className='title'>Vestibulum sapien odio</h3>
-          <p>Vestibulum sapien odio, interdum nec odio faucibus, tincidunt porttitor enim. Nunc blandit neque quis suscipit placerat.</p>
-        </div>
-      </li>
-      <li className='verticalTimeline-item'>
-        <div className='verticalTimeline-image green'>
-          <span className='glyphicon glyphicon-ok' aria-hidden='true' />
-        </div>
-        <div className='verticalTimeline-content'>
-          <h3 className='title'>Nam et lacus neque</h3>
-          <p>Nam et lacus neque. Ut dictum massa arcu, non tristique neque laoreet at. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi.</p>
-        </div>
-      </li>
-    </ul>
+          <li className="verticalTimeline-item">
+            <a href={line.link}>
+                <div className="verticalTimeline-image blue">
+                  <span className="glyphicon glyphicon-th-large" aria-hidden="true"></span>
+                </div>
+                <div className="verticalTimeline-content">
+                  <h3 className="title" key={i}>{line.title}</h3>
+                  <Row>
+                  <Col sm={3}>
+                    <img key={i} src={line.image} alt="trump sucks" />
+                  </Col>
+                  <Col sm={9}>
+                    <p key={i}>{line.description}</p>
+                  </Col>
+                  </Row>
+                </div>
+                  </a>
+              </li>
+
+      ))}
+  </ul>
 
   </Col>
 )
