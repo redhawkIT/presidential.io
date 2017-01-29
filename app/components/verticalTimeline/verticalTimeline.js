e974430a6eaceb9bdf0a08ec0fa3704aae35579f
J. Benjamin Leeds <leeds@uw.edu>
2017-01-28 17:19:33 -0800

    timeline icons dependent on article category

--- app/components/verticalTimeline/verticalTimeline.js
+++ app/components/verticalTimeline/verticalTimeline.js
@@ -6,7 +6,15 @@ import {Row, Col} from 'react-grid-system'
 // import {CardTitle} from 'material-ui/Card'
 // import Divider from 'material-ui/Divider'
 
-// import Event from './Event/Event'
+import Language from 'material-ui/svg-icons/action/language'
+import Landscape from 'material-ui/svg-icons/image/landscape'
+import School from 'material-ui/svg-icons/social/school'
+import TrendingUp from 'material-ui/svg-icons/action/trending-up'
+import People from 'material-ui/svg-icons/social/people'
+
+//let test = true
+
+//let x = test ? true : false
 
 const VerticalTimeline = ({data}) => (
   <Col style={C.Normalize} sm={12}>
@@ -14,10 +22,20 @@ const VerticalTimeline = ({data}) => (
     <ul className='verticalTimeline'>
       {data.headlines && data.headlines.map((line, i) => (
 
-        <li className='verticalTimeline-item'>
+        <li key={i} className='verticalTimeline-item'>
           <a href={line.link}>
+         
             <div className='verticalTimeline-image blue'>
-              <span className='glyphicon glyphicon-th-large' aria-hidden='true' />
+              <div className='verticalTimeline-badge'>
+
+                { (line.category == "Foreign Policy") && <Language color="white"/>}
+                { (line.category == "Environment") && <Landscape color="white"/>}
+                { (line.category == "Education") && <School color="white"/>}
+                { (line.category == "Economics") && <TrendingUp color="white"/>}
+                { (line.category == "Social Welfare") && <People color="white"/>}
+                
+              </div>
+             }
             </div>
             <div className='verticalTimeline-content'>
               <h3 className='title' key={i}>{line.title}</h3>
